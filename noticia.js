c5b1a617b5176ac280f2411ddc5603be5ad0a0ef
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-22 17:56:18 +0000

    Exemplo

--- noticia.js
+++ noticia.js
@@ -4,6 +4,11 @@ function lerNoticias() {
         cartao.className = "cartao";
         cartao.innerHTML = `
            <img src="${item.imagem}" alt="${item.titulo}" class="imagem">
+           <h2>${item.titulo}</h2>
+           <p>${item.categoria} - ${item.data}</p>
+           <p>${item.descricao}</p>
+           <p>${item.autor}</p>
+           <a href="detalhe.html?id=${item.id}">Leia mais</a>
         `;
 
     }); 
