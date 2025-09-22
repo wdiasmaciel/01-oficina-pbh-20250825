1b8604616a7cc44f6b7be1426ce459f2235ed801
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-22 17:39:36 +0000

    Exemplo

--- bd.js
+++ bd.js
@@ -4,35 +4,35 @@ const usuario = {
 };
 
 const dados = [
-  {
-    "id": 1,
-    "titulo": "Mobilidade Urbaba em Belo Horizonte",
-    "descricao": "A mobilidade urbana em Belo Horizonte enfrenta grandes desafios.",
-    "conteudo": "A mobilidade urbana em Belo Horizonte enfrenta grandes desafios, como o congestionamento, a queda na qualidade do transporte público e a predominância do transporte individual. A prefeitura e o governo federal têm anunciado investimentos e projetos para tentar melhorar a situação. ",
-    "categoria": "Transporte",
-    "autor": "Ana Maria Silva",
-    "data": "22/09/2025",
-    "imagem": "assets/imagem/onibus.png"
-  },
-  {
-    "id": 2,
-    "titulo": "Tecnologia 6G",
-    "descricao": "A tecnologia 6G é a sexta geração de redes móveis",
-    "conteudo": "A tecnologia 6G é a sexta geração de redes móveis, prometendo superar as limitações do 5G com velocidades ultrarrápidas (até 1 Tbps), latência de microssegundos, e uma capacidade massiva de conectar dispositivos. Em desenvolvimento e fase de testes em laboratórios globais, o 6G visa ser a base para inovações como comunicação holográfica, gêmeos digitais e a integração de inteligência artificial, com a expectativa de sua chegada comercial por volta de 2030. ",
-    "categoria": "Tecnologia",
-    "autor": "José Campos Alves",
-    "data": "21/09/2025",
-    "imagem": "assets/imagem/tecnologia-6g.png"
-  },
-  {
-    "id": 3,
-    "titulo": ,
-    "descricao": ,
-    "conteudo": ,
-    "categoria": ,
-    "autor": ,
-    "data": ,
-    "imagem": "assets/imagem/"
-  }
+    {
+        "id": 1,
+        "titulo": "Mobilidade Urbaba em Belo Horizonte",
+        "descricao": "A mobilidade urbana em Belo Horizonte enfrenta grandes desafios.",
+        "conteudo": "A mobilidade urbana em Belo Horizonte enfrenta grandes desafios, como o congestionamento, a queda na qualidade do transporte público e a predominância do transporte individual. A prefeitura e o governo federal têm anunciado investimentos e projetos para tentar melhorar a situação. ",
+        "categoria": "Transporte",
+        "autor": "Ana Maria Silva",
+        "data": "22/09/2025",
+        "imagem": "assets/imagem/onibus.png"
+    },
+    {
+        "id": 2,
+        "titulo": "Tecnologia 6G",
+        "descricao": "A tecnologia 6G é a sexta geração de redes móveis",
+        "conteudo": "A tecnologia 6G é a sexta geração de redes móveis, prometendo superar as limitações do 5G com velocidades ultrarrápidas (até 1 Tbps), latência de microssegundos, e uma capacidade massiva de conectar dispositivos. Em desenvolvimento e fase de testes em laboratórios globais, o 6G visa ser a base para inovações como comunicação holográfica, gêmeos digitais e a integração de inteligência artificial, com a expectativa de sua chegada comercial por volta de 2030. ",
+        "categoria": "Tecnologia",
+        "autor": "José Campos Alves",
+        "data": "21/09/2025",
+        "imagem": "assets/imagem/tecnologia-6g.png"
+    },
+    {
+        "id": 3,
+        "titulo": "Festival de Música",
+        "descricao": "Variedade de festivais de música confirmados para 2025 em Belo Horizonte",
+        "conteudo": "Savassi Festival: Um evento com programação de jazz e música instrumental em diversos pontos da cidade.",
+        "categoria": "Cultura",
+        "autor": "Miguel Souza Gonçalves",
+        "data": "20/09/2025",
+        "imagem": "assets/imagem/festival-de-musica.png"
+    }
 ];
 
