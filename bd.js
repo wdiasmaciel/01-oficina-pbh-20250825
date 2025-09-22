70eaaeb23f470f4844368897ea842c7caca62864
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-22 17:33:21 +0000

    Exemplo

--- bd.js
+++ bd.js
@@ -6,13 +6,13 @@ const usuario = {
 const dados = [
   {
     "id": 1,
-    "titulo": ,
-    "descricao": ,
-    "conteudo": ,
-    "categoria": ,
-    "autor": ,
-    "data": ,
-    "imagem": "assets/imagem/"
+    "titulo": "Mobilidade Urbaba em Belo Horizonte",
+    "descricao": "A mobilidade urbana em Belo Horizonte enfrenta grandes desafios.",
+    "conteudo": "A mobilidade urbana em Belo Horizonte enfrenta grandes desafios, como o congestionamento, a queda na qualidade do transporte público e a predominância do transporte individual. A prefeitura e o governo federal têm anunciado investimentos e projetos para tentar melhorar a situação. ",
+    "categoria": "Transporte",
+    "autor": "Ana Maria Silva",
+    "data": "22/09/2025",
+    "imagem": "assets/imagem/onibus.png"
   },
   {
     "id": 2,
